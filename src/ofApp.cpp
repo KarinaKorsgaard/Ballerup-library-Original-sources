7df33482c37bfeae32bef0d171f220a44d9d5831
karinakorsgaard <kkorsgaardjensen@gmail.com>
2018-03-20 12:01:10 +0100

    faces

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,34 +4,15 @@
 void ofApp::setup(){
     gui.setup();
     gui.add(animationTime.set("AnimationTime",1.,1.,5000.));
+    gui.add(alphaTransitionTime.set("alphaTransitionTime",1.,100.,500.));
+    gui.add(scaleFace.set("scaleFace",0.1,0.,2.));
+    gui.add(facePos.set("facePos",ofVec2f(0,0),ofVec2f(0,0),ofVec2f(ofGetWidth(),ofGetHeight())));
+    
     gui.loadFromFile("settings.xml");
-    
-    // serial
-    serial.listDevices();
-    vector <ofSerialDeviceInfo> deviceList = serial.getDeviceList();
-    
-    // this should be set to whatever com port your serial device is connected to.
-    // (ie, COM4 on a pc, /dev/tty.... on linux, /dev/tty... on a mac)
-    // arduino users check in arduino app....
-    int baud = 9600;
-#ifdef __APPLE__
-    serial.setup(0, baud); //open the first device
-    cout << "on mac"<< endl;
-#elif
-    serial.setup("COM4", baud); // windows example
-    cout << "on windows"<< endl;
-#endif
-    //serial.setup("COM4", baud); // windows example
-    //serial.setup("/dev/tty.usbserial-A4001JEC", baud); // mac osx example
-    //serial.setup("/dev/ttyUSB0", baud); //linux example
-    
-    nTimesRead = 0;
-    nBytesRead = 0;
-    readTime = 0;
-    memset(bytesReadString, 0, 4);
-    //    //serial
-    
+
     font.load("circular.ttf", 20);
+    fontSmall.load("circular.ttf", 15);
+    
     charHeight = font.getStringBoundingBox("Åg", 0, 0).height;
     // json crap
     std::string file = "example.json";
@@ -55,36 +36,47 @@ void ofApp::setup(){
     ofTexture tex;
     ofLoadImage(tex, "other/alphamask.png");
     
+    vector<string>assets;
+    assets.push_back("nose");
+    
+    assets.push_back("eyeLeft");
+    assets.push_back("eyeRight");
+    assets.push_back("mouthLower");
+    assets.push_back("mouthUpper");
+    
+    faceCoords.push_back(ofRectangle(0,344-278,124,200));
+    faceCoords.push_back(ofRectangle(faceCoords[0].x-100,0,184,120));
+    faceCoords.push_back(ofRectangle(faceCoords[0].x+100,0,184,120));
+    faceCoords.push_back(ofRectangle(0-20,549-278,276,133));
+    faceCoords.push_back(ofRectangle(0-20,449-278,276,153));
+    
+    
+    
+    
     for(int i = 0; i<quotes.size();i++){
         
         const Json::Value& q = result[i];
         string str = q["quote"].asString();
+        
         int posy = charHeight/2 + font.getLineHeight()/2 - 5;
         ofFbo fbo;
         fbo.allocate(400, charHeight*2, GL_RGBA);
-        
         ofRectangle r = font.getStringBoundingBox(str, 0, 0);
-        
         int y = charHeight/2 - r.height/2 + font.getLineHeight()/2 + 3;
-        
         fbo.begin();
         ofClear(0);
-        
         ofSetColor(255);
         font.drawString(str, 30, y);
-        
         ofNoFill();
         ofSetLineWidth(0.5);
-        
         fbo.end();
-        
         ofBufferObject buffer;
         buffer.allocate(fbo.getWidth() * fbo.getHeight() * 4, GL_STATIC_DRAW);
         fbo.getTexture().copyTo(buffer);
         
+        quotes[i].str = str;
         quotes[i].tex.allocate(fbo.getWidth(), fbo.getHeight(), GL_RGBA);
         quotes[i].tex.loadData(buffer, GL_RGBA, GL_UNSIGNED_BYTE);
-
         quotes[i].img.load("img/"+q["imageName"].asString());
         
         int w = quotes[i].img.getWidth();
@@ -93,8 +85,17 @@ void ofApp::setup(){
         w = w * tempH/h;
         quotes[i].img.resize(w,tempH);
         quotes[i].img.getTexture().setAlphaMask(tex);
-        
         quotes[i].quoteID = i;
+        
+        quotes[i].face.resize(assets.size());
+        for(int u = 0; u<assets.size();u++){
+            string name =ofSplitString(q["imageName"].asString(), ".")[0];
+            string path = ofToDataPath("cutouts/"+name+"-"+assets[u]+".png");
+            if(ofFile::doesFileExist(path))
+               quotes[i].face[u].load(path);
+            else
+                quotes[i].face[u].load("cutouts/blank.png");
+        }    
     }
     
     
@@ -112,43 +113,21 @@ void ofApp::setup(){
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    
+    
     ofSetWindowTitle(ofToString(ofGetFrameRate()));
     
-    int input = -1;
-    nTimesRead = 0;
-    nBytesRead = 0;
-    int nRead  = 0;  // a temp variable to keep count per read
-    
-    unsigned char bytesReturned[3];
-    
-    memset(bytesReadString, 0, 4);
-    memset(bytesReturned, 0, 3);
-    
-    while( (nRead = serial.readBytes( bytesReturned, 3)) > 0){
-        nTimesRead++;
-        nBytesRead = nRead;
-    };
-    
-    if(nBytesRead>0){
-        memcpy(bytesReadString, bytesReturned, 3);
-        
-        bSendSerialMessage = false;
-        readTime = ofGetElapsedTimef();
-        
-        string fromArduino = string(bytesReadString);
-        char fa = fromArduino[0];
-        if(int(fa>15)){
-            input = ofToInt(fromArduino);
-           // cout << fa<<" "<<ofToInt(fromArduino)<<" "<<int(fa) << " bytes "<< nBytesRead << " time " << nTimesRead << endl;
-        }
-    }
+    isInitialized = serial.isInitialized();
+    if(!isInitialized)initialiseArdiono();
+    else readArduino();
     
     if(input!=-1){
         if(input == 6){
-            // do output
+            doOutput();
         }
-        else {
-            input = MIN(input , slots.size()-1);
+        else if (input<slots.size()){
+            
+            isDone = false;
             if(!slots[input].isAnimated){
                 int temp = slots[input].quote->quoteID;
                 while(temp == slots[input].new_quote->quoteID)
@@ -157,25 +136,52 @@ void ofApp::update(){
         }
     }
     
+    
     for(int i = 0; i<slots.size();i++){
-        if(slots[i].quote->quoteID!=slots[i].new_quote->quoteID){
-            animateSlots(&slots[i]);
-        }
-        else {
-            slots[i].isAnimated = false;
-        }
+        animateSlots(&slots[i]);
     }
+    
+    if(!isDone){
+        if(transitionAlpha>0)
+            transitionAlpha-=ofGetLastFrameTime()*alphaTransitionTime;
+    }
+    else if (isDone && slots[0].xpos<-ofGetWidth()){
+        if(transitionAlpha<254)
+            transitionAlpha+=ofGetLastFrameTime()*alphaTransitionTime;
+    }
+    
+}
+void ofApp::doOutput(){
+    if(!isDone){
+        finalSpeech.clear();
+        string temp = "";
+        for(int i = 0; i<slots.size();i++){
+            string str = slots[i].quote->str;
+            for(int u = 0; u<str.length();u++){
+                temp.append(str.substr(u,1));
+                
+                ofRectangle r = font.getStringBoundingBox(temp, 0, 0);
+                if(r.width>collumWidth && str.substr(u,1)==" "){
+                    finalSpeech.push_back(temp);
+                    cout<< finalSpeech.back()<<endl;
+                    temp = "";
+                }
+            }
+            if(i!=slots.size()-1)
+                temp.append(", ");
+            
+            //finalSpeech.push_back(temp);
+            //cout<< finalSpeech.back()<<endl;
+        }
+        finalSpeech.push_back(temp);
+        finalSpeech.back().append(".");
+        
+        
+    }
+    
+    isDone = true;
 }
 
-void ofApp::animateSlots(slot * s){
-    s->ypos -= ofGetLastFrameTime()*animationTime;
-    s->isAnimated = true;
-   
-    if(s->ypos < -s->quote->tex.getHeight()){
-        s->ypos = 20;
-        s->quote=&quotes[(s->quote->quoteID+1) % quotes.size()];
-    }
-}
 
 //--------------------------------------------------------------
 void ofApp::draw(){
@@ -193,27 +199,169 @@ void ofApp::draw(){
         int h = slots[i].quote->img.getHeight();
         int tempH = charHeight*2;
         w = w * tempH/h;
-        slots[i].quote->img.draw(0, 0, w, tempH);
+        slots[i].quote->img.draw(slots[i].xpos + 0, 0, w, tempH);
         
-        roundedRect(xpos_rect , 0, 500, charHeight*2, charHeight);
+        roundedRect(slots[i].xpos + xpos_rect , 0, 500, charHeight*2, charHeight);
         
         ofSetColor(0);
         
-        slots[i].quote->tex.draw(xpos_rect, slots[i].ypos);
+        slots[i].quote->tex.draw(slots[i].xpos + xpos_rect, slots[i].ypos);
         if(slots[i].isAnimated){
             Quote q = quotes[slots[i].quote->quoteID % quotes.size()];
-            q.tex.draw(xpos_rect, q.tex.getHeight() + slots[i].ypos);
+            q.tex.draw(slots[i].xpos + xpos_rect, q.tex.getHeight() + slots[i].ypos);
         }
         ofTranslate(0, charHeight*4);
     }
     ofPopMatrix();
     
+    if(isDone){
+
+        ofSetColor(transitionAlpha);
+
+        int lineH = font.getLineHeight();
+        if(slots.back().xpos<-ofGetWidth()){
+            for(int i = 0; i<finalSpeech.size();i++)
+            font.drawString(finalSpeech[i], ofGetWidth()/2, (ofGetHeight()/2-finalSpeech.size()*lineH/2) + i*lineH);
+            drawFace();
+        }
+
+        ofRectangle r = fontSmall.getStringBoundingBox("press print to print your speech,", 0, 0);
+        fontSmall.drawString("press print to print your speech,",ofGetWidth()/2-r.width/2,ofGetHeight()-100);
+        r = fontSmall.getStringBoundingBox("or press a button to restart!", 0, 0);
+        fontSmall.drawString("or press a button to restart!",ofGetWidth()/2-r.width/2,ofGetHeight()-70);
+        
+        
+    }
+    
     gui.draw();
 }
+//--------------------------------------------------------------
+void ofApp::drawFace(){
+    ofPushMatrix();
+    ofTranslate(facePos->x, facePos->y);
+    ofScale(scaleFace, scaleFace);
+    
+    bool blink = ofRandom(2)<0.1;
+    if(blink)lowerMouth.y = sin(ofGetElapsedTimef()*10)*50;
+    lowerMouth.y = lowerMouth.y>0 ? 50:0;
 
+    for(int i = slots.size()-1; i>-1; i--){
+        ofImage img = slots[i].quote->face[i];
+        float a = img.getWidth()/img.getHeight();
+        
+        float h = faceCoords[i].height;
+        float w = h*a;
+        //if((i==1 || i==2)&&blink)h=0;
+        
+        float x = faceCoords[i].x;
+        float y = faceCoords[i].y;
+        if(i==3)y+=lowerMouth.y;
+        
+        img.draw(x,y,w,h);
+    }
+    ofPopMatrix();
+}
+//--------------------------------------------------------------
+void ofApp::animateSlots(slot * s){
+    if (!isDone){
+        //animate quote pos
+        if(s->quote->quoteID!=s->new_quote->quoteID) {
+            s->ypos -= ofGetLastFrameTime()*animationTime;
+            s->isAnimated = true;
+            if(s->ypos < -s->quote->tex.getHeight()){
+                s->ypos = 20;
+                s->quote=&quotes[(s->quote->quoteID+1) % quotes.size()];
+            }
+        } else {
+            s->isAnimated = false;
+        }
+        //animate back in
+        if(s->xpos<0) {
+            float t = ofGetElapsedTimef()-s->animateTime;
+            s->xpos = easeInOut_s(t, -ofGetWidth(), ofGetWidth(), 2, 1);
+        }else{
+            s->animateTime = ofGetElapsedTimef();
+        }
+        if(transitionAlpha>0)
+            transitionAlpha-=ofGetLastFrameTime();
+    }
+    else {
+        if(s->xpos>-ofGetWidth()) {
+            float t = ofGetElapsedTimef()-s->animateTime;
+            s->xpos = easeInOut_s(t, 0 , -ofGetWidth(), 2, 1);
+        }else{
+            s->animateTime = ofGetElapsedTimef();
+        }
+    }
+}
+//--------------------------------------------------------------
+void ofApp::initialiseArdiono(){
+    serial.listDevices();
+    vector <ofSerialDeviceInfo> deviceList = serial.getDeviceList();
+    
+    // this should be set to whatever com port your serial device is connected to.
+    // (ie, COM4 on a pc, /dev/tty.... on linux, /dev/tty... on a mac)
+    // arduino users check in arduino app....
+    int baud = 9600;
+#ifdef __APPLE__
+    serial.setup(0, baud); //open the first device
+    cout << "on mac"<< endl;
+#elif
+    serial.setup("COM4", baud); // windows example
+    cout << "on windows"<< endl;
+#endif
+    //serial.setup("COM4", baud); // windows example
+    //serial.setup("/dev/tty.usbserial-1421", baud); // mac osx example
+    //serial.setup("/dev/ttyUSB0", baud); //linux example
+    
+    nTimesRead = 0;
+    nBytesRead = 0;
+    readTime = 0;
+    memset(bytesReadString, 0, 4);
+    
+    isInitialized = serial.isInitialized();
+    if(isInitialized)cout << "arduino is on"<<endl;
+}
+void ofApp::readArduino(){
+    input = -1;
+    nTimesRead = 0;
+    nBytesRead = 0;
+    int nRead  = 0;  // a temp variable to keep count per read
+    
+    unsigned char bytesReturned[3];
+    
+    memset(bytesReadString, 0, 4);
+    memset(bytesReturned, 0, 3);
+    
+    while( (nRead = serial.readBytes( bytesReturned, 3)) > 0){
+        nTimesRead++;
+        nBytesRead = nRead;
+    };
+    
+    //if(nBytesRead>0){
+        memcpy(bytesReadString, bytesReturned, 3);
+        
+        bSendSerialMessage = false;
+        readTime = ofGetElapsedTimef();
+        
+        string fromArduino = string(bytesReadString);
+        char fa = fromArduino[0];
+        if(int(fa>15)){
+            input = ofToInt(fromArduino);
+            //cout << fa<<" "<<ofToInt(fromArduino)<<" "<<int(fa) << " bytes "<< nBytesRead << " time " << nTimesRead << endl;
+        }
+   // }
+    if(fromArduino!="")cout << fromArduino << endl;
+}
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
+    if(key == 'd')doOutput();
     
+    if(key-'0'<slots.size()){
+       slots[key-'0'].quote = &quotes[int(ofRandom(quotes.size()))];
+        
+    }
+
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -6,8 +6,11 @@
 
 struct Quote{
     ofTexture tex;
+    string str;
     ofImage img;
     int quoteID;
+    
+    vector<ofImage>face;
 
 };
 
@@ -16,7 +19,8 @@ struct slot{
     Quote * new_quote;
     bool isAnimated;
     double ypos = 0.0;
-    
+    double xpos = 0.0;
+    double animateTime = 0.0;
 };
 
 class ofApp : public ofBaseApp{
@@ -39,14 +43,30 @@ class ofApp : public ofBaseApp{
 		void gotMessage(ofMessage msg);
 		
     ofTrueTypeFont font;
+    ofTrueTypeFont fontSmall;
     vector<Quote>quotes;
     vector<slot>slots;
+    vector<ofRectangle>faceCoords;
+    
+    void doOutput();
+    void drawFace();
+
+    
+    vector<string> finalSpeech;
+    int collumWidth = 400;
 
     int charHeight;
     void animateSlots(slot * s);
     
     ofxPanel gui;
     ofParameter<float>animationTime;
+    ofParameter<float>alphaTransitionTime;
+    ofParameter<float>scaleFace;
+    ofParameter<ofVec2f>facePos;
+    ofVec2f lowerMouth = ofVec2f(0,0);
+    
+    bool isDone = false;
+    float transitionAlpha;
     
     bool        bSendSerialMessage;            // a flag for sending serial
     char        bytesRead[3];                // data from serial, we will be trying to read 3
@@ -54,8 +74,13 @@ class ofApp : public ofBaseApp{
     int            nBytesRead;                    // how much did we read?
     int            nTimesRead;                    // how many times did we read?
     float        readTime;                    // when did we last read?
-    
+    bool isInitialized = false;
+    void initialiseArdiono();
+    void readArduino();
+    int input = -1; 
     ofSerial    serial;
+    
+
     
     Quote getRandomQuote(){
         return quotes[int(ofRandom(quotes.size()))];
@@ -84,4 +109,13 @@ class ofApp : public ofBaseApp{
         // finally call cubic Bezier curve function
         ofBezierVertex(cp1x, cp1y, cp2x, cp2y, x, y);
     };
+    
+    inline static float easeInOut_s(float t, float b , float c, float d, float s) {
+        s*=(1.525f);
+        if ((t/=d/2) < 1){
+            return c/2*(t*t*((s+1)*t - s)) + b;
+        }
+        float postFix = t-=2;
+        return c/2*((postFix)*t*((s+1)*t + s) + 2) + b;
+    }
 };
